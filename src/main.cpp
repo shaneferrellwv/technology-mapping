5c2a6395fde2fdbb252a486e39b5afdad6f75525
Devin Willis <gatordevin@gmail.com>
2023-11-27 21:15:16 -0500

    Fixed graph processing code and started work on the library

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,38 @@ const int infinity = 99999999;
 
 int main(int argc, char* argv[])
 {
-    string fileName = argv[1];
+    // take input from user or if empty default to input/input1.net which is a txt
+    // file containing the netlist
+    string fileName = "";
+    cout << "Enter the name of the file containing the netlist: ";
+    getline(cin, fileName);
+
+    // if no file name is given, use the default input/input1.net
+    if(fileName == "")
+    {
+        fileName = "input1.net";
+    }
 
     try
     {
+        Library* library = new Library();
+
         Node* andOrNotDAGRoot = Node::constructAndOrNotDAG(fileName);
+        Node::outputTruthTable(andOrNotDAGRoot);
+        cout << endl;
         Node* nandNotDAGRoot = Node::constructNandNotDAG(andOrNotDAGRoot);
+        Node::outputTruthTable(nandNotDAGRoot);
+        cout << endl;
         nandNotDAGRoot = Node::simplify(nandNotDAGRoot);
+        Node::outputTruthTable(nandNotDAGRoot);
+        cout << endl;
+
+        // Find matching sturctures
+        vector<string> structures = library->findMatchingStructures(nandNotDAGRoot);
+        for(int i = 0; i < structures.size(); i++)
+        {
+            cout << structures[i] << endl;
+        }
     }
     catch(const exception& e)
     {
